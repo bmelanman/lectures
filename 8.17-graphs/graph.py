24efe2000f81b62e40d99cda617e6dca471f5b1c
Christopher Siu <christopher@siu.sh>
2022-08-19 11:17:09 -0700

    Added adjacency matrix pseudocode.

--- 8.17-graphs/graph.py
+++ 8.17-graphs/graph.py
@@ -9,13 +9,20 @@ class Graph:
         self.matrix = dct.Dictionary()
         # The number of edges in this graph:
         self.size = 0
+        # NOTE: The number of vertices is simply the number of keys.
 
 
 def add_vertex(graph, vertex):
+    # Insert the vertex into the matrix, mapped to a new, empty dictionary.
     pass
 
 
 def add_edge(graph, vertex_u, vertex_v):
+    # Get vertex_u's dictionary from the matrix.
+    # Insert vertex_v into vertex_u's dictionary, mapped to 1.
+    # Get vertex_v's dictionary from the matrix.
+    # Insert vertex_u into vertex_v's dictionary, mapped to 1.
+    # Increment the size.
     pass
 
 
