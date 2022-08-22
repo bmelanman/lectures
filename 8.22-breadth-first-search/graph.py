3f2dbc3e5c9ae521298bcfe470507e6a4a521c43
Christopher Siu <christopher@siu.sh>
2022-08-21 18:37:50 -0700

    Added base BFS example.

---
+++ 8.22-breadth-first-search/graph.py
@@ -0,0 +1,29 @@
+import dictionary as dct
+
+
+class Graph:
+    """ A collection of vertices and edges """
+
+    def __init__(self):
+        # The backing adjacency matrix:
+        self.matrix = dct.Dictionary()
+        # The number of edges in this graph:
+        self.size = 0
+
+
+def add_vertex(graph, vertex):
+    # Insert the vertex into the matrix, mapped to a new, empty dictionary.
+    pass
+
+
+def add_edge(graph, vertex_u, vertex_v):
+    # Get vertex_u's dictionary from the matrix.
+    # Insert vertex_v into vertex_u's dictionary, mapped to 1.
+    # Get vertex_v's dictionary from the matrix.
+    # Insert vertex_u into vertex_v's dictionary, mapped to 1.
+    # Increment the size.
+    pass
+
+
+def path(graph, vertex_u, vertex_v):
+    pass
