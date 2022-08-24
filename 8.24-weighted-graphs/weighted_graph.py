0a29e05d8cc6fdcdbbe38901dc8f5d0b9e4eb358
Christopher Siu <christopher@siu.sh>
2022-08-24 11:16:18 -0700

    Added Dijkstra's algorithm pseudocode.

--- 8.24-weighted-graphs/weighted_graph.py
+++ 8.24-weighted-graphs/weighted_graph.py
@@ -1,8 +1,8 @@
 import dictionary as dct
 
 
-class Graph:
-    """ A collection of vertices and edges """
+class WeightedGraph:
+    """ A collection of vertices and weighted edges """
 
     def __init__(self):
         # The backing adjacency matrix:
@@ -16,28 +16,35 @@ def add_vertex(graph, vertex):
     pass
 
 
-def add_edge(graph, vertex_u, vertex_v):
+def add_edge(graph, vertex_u, vertex_v, weight):
     # Get vertex_u's dictionary from the matrix.
-    # Insert vertex_v into vertex_u's dictionary, mapped to 1.
+    # Insert vertex_v into vertex_u's dictionary, mapped to weight.
     # Get vertex_v's dictionary from the matrix.
-    # Insert vertex_u into vertex_v's dictionary, mapped to 1.
+    # Insert vertex_u into vertex_v's dictionary, mapped to weight.
     # Increment the size.
     pass
 
 
 def path(graph, vertex_u, vertex_v):
-    # Create an empty queue of jobs.
-    # Enqueue vertex_u to the queue of jobs.
+    # Create an empty priority queue of jobs.
+    # Enqueue (0, vertex_u) to the priority queue of jobs.
     # Create an empty dictionary of predecessors.
     # Insert (vertex_u, vertex_u) into the dictionary of predecessors.
-    # While the queue of jobs is non-empty, do:
-    #     Dequeue a current vertex from the queue of jobs.
-    #     If the current vertex is vertex_v, then:
+    # Create an empty dictionary of distances.
+    # Insert (vertex_u, 0) into the dictionary of distances.
+    # While the priority queue of jobs is non-empty, do:
+    #     Dequeue a (priority, vertex) from the priority queue of jobs.
+    #     Set a temporary variable to -1 * priority.
+    #     If temp > the distance to the vertex, then:
+    #         (there is already a better path; continue and ignore this one)
+    #     Else if the current vertex is vertex_v, then:
     #         (the path to vertex_v is in the dictionary; return it)
     #     Get the current vertex's dictionary from the matrix.
     #     For each key (a neighbor) in the current vertex's dictionary, do:
-    #         If the neighbor is not in the dictionary of predecessors, then:
-    #             Enqueue the neighbor to the queue of jobs.
-    #             Insert (neighbor, current) into the dictionary.
+    #         If the neighbor has no predecessor or if has a distance such that
+    #          distance > temp + weight, then:
+    #             Enqueue (-1 * (temp + weight), neighbor).
+    #             Insert (neighbor, current) into the predecessors dict.
+    #             Insert (neighbor, temp + weight) into the distances dict.
     # (there is no path to vertex_v)
     pass
